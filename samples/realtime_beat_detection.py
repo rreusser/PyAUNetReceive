49e27896b01c1b1dcf66dfee4d03449fceba138b
Ricky Reusser <rsreusser@gmail.com>
2014-03-09 20:36:19 -0400

    Realtime tempo detection

--- samples/realtime_beat_detection.py
+++ samples/realtime_beat_detection.py
@@ -1,22 +1,38 @@
-import math
 import numpy as np
-from pyaunetreceive import AUNetReceive
-import matplotlib.pyplot as pl
 
 
 def hamming_window(n):
-  0.54 - 0.46*np.cos(2.0*math.pi*np.arange(n)/(n-1.0))
+  return 0.54 - 0.46*np.cos(2.0*np.pi*np.arange(n)/(n-1.0))
 
-def logspectrum(y):
-  
-  return np.log(np.abs(np.fft.rfft(y)) + 1.0e-13)
+def absspectrum(y,window):
+  global debug
+  d = np.abs(np.fft.rfft(y*window))
+  #d -= np.mean(d)
+  return d
 
-def summed_logspectrum(l,r):
-  return logspectrum(l) + logspectrum(r)
+def summed_spec(l,r,window):
+  global debug
+  sumspec = absspectrum(l,window) + absspectrum(r,window)
+  return np.log(sumspec +1e-13) #* np.arange(len(sumspec))
 
-def correlation_function(a,b):
-  return np.dot(a,b)/(np.linalg.norm(a)+1.0e-13)/(np.linalg.norm(b)+1.0e-13)
 
+debug = None
+
+
+class AudioBuffer:
+  '''Eats audio data and executes a callback when a certain amount of data is
+  accumulated.'''
+
+  def __init__(s, num_bytes=1024):
+    s.buf = ''
+    s.bufsize = num_bytes
+    s.callback = lambda x: None
+
+  def buffer(s,data):
+    s.buf += data
+    if len(s.buf) >= s.bufsize:
+      s.callback( s.buf[0:s.bufsize] )
+      s.buf = s.buf[s.bufsize:]
 
 
 class BeatDetector:
@@ -57,22 +73,22 @@ class BeatDetector:
 
     # A reference to the function with which we compute the spectrum of each segment
     # of data:
-    s.spectrum_function = None
+    s.spectrum_function = summed_spec
     
     # The number of running samples with which to compare the new sample
-    s.memory = 256
+    s.memory = 512
 
     # This is the beat spectrum in which we accumulate the data!
     s.beat_spectrum = np.zeros(s.memory)
 
     # The number of samples considered at a time in the audio input:
-    s.window_width = 256
+    s.window_width = 1024
 
     # The number of samples expected in the audio input:
-    s.samples_per_input = 256
+    s.samples_per_input = 1024
 
     # Precalculate a window function for the fft:
-    s.window_func = hamming_window(s.window_width)
+    s.window = hamming_window(s.window_width)
 
     # Just precompute this since we reference it a bunch:
     s.window_halfwidth = s.window_width//2
@@ -90,20 +106,20 @@ class BeatDetector:
     s.sample_freq = 44100
 
     # Characteristic decay time of the beat spectrum:
-    s.halflife = 1.0
+    s.halflife = 2.5
 
     # Width of a window of data, in seconds:
     s.window_dt = 1.0*s.window_width / s.sample_freq
 
     # The window shifts by half of the window width due to the half-window overlap,
     # so the time constant accounts for this:
-    s.decay_factor = np.exp( -np.log(2.0) * ( s.window_dt * 0.5 ) )
-    #print 'decay factor=',s.decay_factor
+    s.decay_factor = np.exp( -np.log(2.0) * ( s.window_dt * 0.5 ) / s.halflife )
 
     s.l_halfprev = None
     s.r_halfprev = None
 
   def assimilate(s,l,r):
+    global debug
     '''Assimilate new stereo input into the running beat spectrum. This function expects
     samples that are an integral multiple of the window width, and frankly it'd probably
     be a really smart thing to do to just use a power of two. Srsly.'''
@@ -132,7 +148,7 @@ class BeatDetector:
         r_input = r[offset:offset+s.window_width]
 
       # Once we have our input data, apply the spectrum function to it:
-      spec = s.spectrum_function(l_input,r_input)
+      spec = s.spectrum_function(l_input,r_input, s.window)
 
       # Our similarity metric between spectra A and B is:
       #
@@ -144,7 +160,7 @@ class BeatDetector:
       spec /= np.linalg.norm(spec)
 
       # Okay, so 'spec' is the new spectrum that's being assimilated into the beat
-      # beat detector's current state:
+      # detector's current state:
       s._append_to_spectra( spec )
 
       # Finally, shift the window and do the whole thing again if there are more sample
@@ -154,8 +170,8 @@ class BeatDetector:
 
     # Store a reference to the previous data so that we can half-overlap the previous
     # window's data
-    s.l_halfprev = l[s.window_halfwidth:]
-    s.r_halfprev = r[s.window_halfwidth:]
+    s.l_halfprev = l[-s.window_halfwidth:]
+    s.r_halfprev = r[-s.window_halfwidth:]
 
 
   def _append_to_spectra(s,v):
@@ -172,12 +188,12 @@ class BeatDetector:
     # Note: uses broadcasting to perform per-column multiplication of a vector without
     # actually tiling the comparison vector into a full matrix. Also note that it's all
     # real magnitude data so we can neglect the complex conjuwhatnot.
-    rolled_correlation = np.dot( s.circular_buffer, v )
+    rolled_correlation = np.dot( s.circular_buffer, v ) / np.linalg.norm(v)
 
     # The correlation has an offset determined by the cirular buffer's current position,
     # so we'll roll the array so that the first element of the correlations is always
     # the autocorrelation:
-    correlation = np.roll( rolled_correlation, -s.circular_buffer_position )
+    correlation = np.roll( rolled_correlation, -s.circular_buffer_position-1 )
 
     s.beat_spectrum *= s.decay_factor
     s.beat_spectrum += correlation
@@ -188,39 +204,62 @@ class BeatDetector:
 
 
 
-beat_detector = BeatDetector()
-beat_detector.spectrum_function = summed_logspectrum
+if __name__ == "__main__":
 
-b=beat_detector
+  from pyaunetreceive import AUNetReceive
+  import matplotlib.pyplot as pl
 
-
-frame = 0
-
-def process_audio_data(data):
-  global beat_detector, frame
-
-  lr = np.fromstring(data,dtype=np.int16)/32768.0
-  l = lr[::2]
-  r = lr[1::2]
-
-  beat_detector.assimilate(l,r)
-
-  if frame == 256:
-    raise KeyboardInterrupt
-
-  frame += 1
-  
+  debug = np.zeros(0)
 
 
+  beat_detector = BeatDetector()
+
+  pl.ion()
+  g = pl.plot(np.arange(beat_detector.memory))[0]
+
+
+  frame = 0
+  def process_buffered_data(data):
+    global g, frame
+    lr = np.fromstring(data,dtype=np.int16)/32768.0
+    l = lr[::2]
+    r = lr[1::2]
+    beat_detector.assimilate(l,r)
+    if frame%2 == 0:
+      y = beat_detector.beat_spectrum[::-1]
+      g.set_ydata(y)
+      pl.axis([0,beat_detector.memory,np.min(y),np.max(y)])
+      pl.draw()
+    frame += 1
+
+
+  audiobuffer = AudioBuffer(4096)
+  audiobuffer.callback = process_buffered_data
+
+
+  def process_audio_data(data):
+    global audiobuffer
+    audiobuffer.buffer(data)
 
 
 
-try:
-  netrecv = AUNetReceive( host='127.0.0.1', port=52800 )
-  netrecv.onrecv( process_audio_data )
-  netrecv.listen()
-except KeyboardInterrupt:
 
-  if False:
-    pl.imshow(np.flipud(np.transpose(np.array(spec))))
-    pl.show()
+
+  try:
+    if True:
+      netrecv = AUNetReceive( host='127.0.0.1', port=52800 )
+      netrecv.onrecv( process_audio_data )
+      netrecv.listen()
+
+    else:
+      f = open('audiodata.pcm','r')
+      d = f.read()
+      for i in range(0,1600):
+        process_audio_data( d[1024*i:1024*(i+1)] )
+      raise KeyboardInterrupt
+
+  except KeyboardInterrupt:
+    if False:
+      pl.show()
+      b=beat_detector
+      pl.plot(b.beat_spectrum)
